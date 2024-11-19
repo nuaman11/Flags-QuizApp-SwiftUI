c34306647e361276fd0c8b0552a2d46c1f5b28e3
nuaman <nuaman.s@axeltechnologies.com>
2024-11-19 12:53:54 +0530

    Initial Commit

---
+++ Flags-QuizApp-SwiftUI/Flags_QuizApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flags_QuizApp_SwiftUIApp.swift
+//  Flags-QuizApp-SwiftUI
+//
+//  Created by Muhammed Nuaman Sadiq on 19/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Flags_QuizApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
